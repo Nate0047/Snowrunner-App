692b619afe035ee46520e291a2032755070f1159
Nathan Birdsall <NBirdsall47@hotmail.co.uk>
2022-12-06 10:14:18 +0000

    Developed app to MVP. All working and stage 1 completed.

--- Snowrunner - tyre comparison.R
+++ Snowrunner - tyre comparison.R
@@ -20,30 +20,35 @@ library(jsonlite)
 if(!require("odbc")) {install.packages("odbc")}
 library(odbc)
 
+# colour palette
+cbPalette <- c("#999999", "#C4A484", "#411900", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-# Connect to db ----------------------------------------------------------------
-connection <-
-odbc::dbConnect(odbc::odbc(),
-  Driver = "SQL Server Native Client 11.0",
-  Server = "tcp:turkey-sql.database.windows.net,1433",
-  Database = "turkey",
-  UID = "nate",
-  PWD = "yadda123!"
-)
+# # Connect to db ----------------------------------------------------------------
+# connection <-
+# odbc::dbConnect(odbc::odbc(),
+#   Driver = "SQL Server Native Client 11.0",
+#   Server = "tcp:turkey-sql.database.windows.net,1433",
+#   Database = "turkey",
+#   UID = "nate",
+#   PWD = "yadda123!"
+# )
+# 
+# # Query db ---------------------------------------------------------------------
+# 
+# # call all truck via stored procedure in db
+# SnowrunnerData <- 
+# odbc::dbGetQuery(connection,
+#                  "Rshiny.GetAllTrucks")
+# 
+# SnowrunnerData %<>%
+#   mutate(across(where(is.character), as.factor))
 
-# Query db ---------------------------------------------------------------------
-
-# call all truck via stored procedure in db
-SnowrunnerData <- 
-odbc::dbGetQuery(connection,
-                 "Rshiny.GetAllTrucks")
-
-SnowrunnerData %<>%
-  mutate(across(where(is.character), as.factor))
-  
+# for when DB doesnt work:
+# write.csv(SnowrunnerData, "dbBackup.csv")
+SnowrunnerData <- read.csv("dbBackup.csv")
 
 # UI ---------------------------------------------------------------------------
-ui <- fluidPage(titlePanel("title panel"),
+ui <- fluidPage(titlePanel("Snowrunner Tyre Comparison App"),
     
     sidebarLayout(
       sidebarPanel(
@@ -66,7 +71,7 @@ ui <- fluidPage(titlePanel("title panel"),
       ),
       
       mainPanel(
-        # complete with visuals later
+         plotOutput("tyrePlot")
       )
     )
   )
@@ -76,6 +81,7 @@ server <- function(input, output, session) {
   
   observeEvent(input$TruckName, {
 
+      # updating tyre selection
       updateSelectInput(
         session,
         input = "TyreName",
@@ -84,8 +90,24 @@ server <- function(input, output, session) {
                                  "Tyre", drop = TRUE]
         )
   })
+    
+      # reactive filtering of dataset for plotting
+      refineTyre <- reactive({
+        SnowrunnerData %>%
+          filter(.$Truck_Name %in% input$TruckName) %>%
+          filter(.$Tyre %in% input$TyreName)
+      })
+      
+      # plotting dataset
+      output$tyrePlot <- renderPlot({
+        ggplot(refineTyre(), aes(x = Tyre, y = Tyre_Coefficient, fill = Tyre_Type)) +
+          geom_bar(stat = "identity", position = "dodge") +
+          scale_fill_manual(values = cbPalette) +
+          ggtitle("Truck Tyre Comparison Chart") +
+          labs(x = "Selected Tyre(s)", y = "Tyre Coefficient") +
+          guides(fill = guide_legend(title = "Tyre Role"))
+      })
   
-
   session$onSessionEnded(stopApp)
 }
 
